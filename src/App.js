0ef2cc871cd27fdc9ac4f11da5deb243b7ee34eb
Ben-Hassine-Imen <imenbenhassine60@gmail.com>
2021-04-14 11:14:29 +0100

    react3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import ProfilPhoto from './component/profil/ProfilPhoto';
+import FullName from './component/profil/FullName';
+import Adress from './component/profil/Adress';
 
-function App() {
+
+
+
+const  App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+  
+       <ProfilPhoto/>
+     
+       <FullName/>
+      
+       <Adress/>
+  
     </div>
   );
 }
 
 export default App;
+
---
+++ src/component/profil/ProfilPhoto.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+const ProfilPhoto = () => {
+    return (
+        <div>
+        <img  className="photo" src="/photo.jpg" alt="myphoto" />
+        </div>
+    );
+   };
+   export default ProfilPhoto;
